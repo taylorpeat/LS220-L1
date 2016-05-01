b164f57e7af82ad735a4737ceecc43166f8788d2
Taylor Peat <taylorpeat@hotmail.com>
2016-05-01 01:46:12 -0400

    logic exercises

---
+++ logic_exercises.js
@@ -0,0 +1,20 @@
+var apples = 3,
+    bananas = 1,
+    either_or = apples || bananas;
+
+console.log(either_or);
+
+    are_equal = apples === bananas;
+
+console.log(are_equal);
+
+
+if (apples === bananas) {
+  console.log("It's true!");
+} else {
+  if (apples == bananas) {
+  console.log("Sort of true!");
+  } else {
+    console.log("Not true...");
+  }
+}
